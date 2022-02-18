200428d448f2e238a3fc0382c5e97147fe8aed97
Aryan Sharma <official.aryansharma01@gmail.com>
2022-02-18 17:41:15 +0530

    [NEW] Added Routing Login & Sign up component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './modules/shared/login/login.component';
+import {SignupComponent} from './modules/shared/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "",
+  pathMatch:"full",
+  redirectTo: "login"
+},{
+  path: "login",
+  component: LoginComponent
+},
+{
+  path: "signup",
+  component: SignupComponent
+},{
+  path: "**",
+  pathMatch: "full",
+  redirectTo: "login"
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './modules/shared/login/login.component';
+import { SignupComponent } from './modules/shared/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
